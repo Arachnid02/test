65028c1e4cc43baed7e6bb9a8dcd8a1d1fb3646c
Arachnid02 <jianwen.chen@uamaker.nyc>
2018-12-02 21:28:39 -0500

    test

---
+++ aliensAFN04.py
@@ -0,0 +1,31 @@
+#aliens.py will take two inputs from the user, the amount of aliens that landed
+#on earth, and how many weeks have elapsed since they landed. With these two
+#inputs, the program will calculate the alien population on earth.
+
+#Fixed grammar issues with the program. Reduced variables that are not used.
+
+startAlien = int(-1)
+elapsedWeeks = int(-1)
+week = str("week")
+
+print("This program will calculate the alien population after receiving two")
+print("inputs from you; the amount of aliens that landed on earth, and how")
+print("many weeks have elapsed since they landed.\n")
+startAlien = int(input("How many aliens landed on earth?\n"))
+elapsedWeeks = int(input("How many weeks have elapsed since they landed?\n"))
+
+if startAlien <= 0:
+    print("Since your inputs are less than 1, mathematically the alien")
+    print("population will make no sense. If you want to try the program with")
+    print("numbers that are greater than 0, please run it again.")
+
+if elapsedWeeks <= 0:
+    print("Since your inputs are less than 1, mathematically the alien")
+    print("population will make no sense. If you want to try the program with")
+    print("numbers that are greater than 0, please run it again.")
+
+for i in range(elapsedWeeks):
+    print("After", i+1, week, "there will be", startAlien * 2 ** (i+1), "aliens.")
+    i+1
+    if i+1 > 0:
+        week = "weeks"
